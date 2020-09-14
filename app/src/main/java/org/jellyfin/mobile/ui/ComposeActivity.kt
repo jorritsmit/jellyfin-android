45af73ba74ec4b6737dc3dbbafbf884c19d21563
Maxr1998 <max.rumpf1998@gmail.com>
2020-09-14 18:28:16 +0200

    Lay groundwork for compose UI

---
+++ app/src/main/java/org/jellyfin/mobile/ui/ComposeActivity.kt
@@ -0,0 +1,41 @@
+package org.jellyfin.mobile.ui
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.runtime.Providers
+import androidx.compose.ui.platform.setContent
+import com.github.zsoltk.compose.backpress.AmbientBackPressHandler
+import com.github.zsoltk.compose.backpress.BackPressHandler
+import com.github.zsoltk.compose.savedinstancestate.BundleScope
+import com.github.zsoltk.compose.savedinstancestate.saveAmbient
+import org.jellyfin.mobile.ui.utils.ContextTheme
+
+class ComposeActivity : AppCompatActivity() {
+
+    private val backPressHandler = BackPressHandler()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BundleScope(savedInstanceState) {
+                Providers(AmbientBackPressHandler provides backPressHandler) {
+                    ContextTheme {
+                        AppContent()
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        outState.saveAmbient()
+    }
+
+    override fun onBackPressed() {
+        if (!backPressHandler.handle()) {
+            super.onBackPressed()
+        }
+    }
+}
